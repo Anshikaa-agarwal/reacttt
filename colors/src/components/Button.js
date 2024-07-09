55839f75aac418d8d2ae50ae7f994eecd37f7c5c
Anshika <anshika2020ag@gmail.com>
2024-07-09 23:50:31 +0530

    btn

--- colors/src/components/Button.js
+++ colors/src/components/Button.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
 import './button.css';
 
-export default function Button() {
+export default function Button(props) {
     const [isClicked, setIsClicked] = useState(false);
+    const [myStyle, setMyStyle] = useState({
+        backgroundColor: props.color
+    })
 
     const handleClick = () => {
+        console.log('Button clicked');
         setIsClicked(!isClicked);
+        console.log('isClicked state:', isClicked);
     }
 
     return (
         <button 
             id="myButton" 
-            className={isClicked ? 'clicked' : ''} 
+            className={isClicked ? 'clicked btn' : 'btn'} 
             onClick={handleClick}
-        >
-            Click Me
-        </button>
+            style={myStyle}
+        ></button>
     );
 }
